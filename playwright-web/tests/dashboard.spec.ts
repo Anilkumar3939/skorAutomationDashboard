708efa430ea36f48f098323d83aae8344889714b
Anil Kumar <anilkumar3939104@gmail.com>
2026-09-14 20:26:33 +0530

    updated test

--- playwright-web/tests/dashboard.spec.ts
+++ playwright-web/tests/dashboard.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Dashboard loads successfully', async ({ page }) => {
+test('dashboard loads successfully', async ({ page }) => {
     await page.goto('/');
 
     await expect(page).toHaveTitle("SKOR Testing Dashboard");
